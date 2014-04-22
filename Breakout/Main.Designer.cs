c0a8165c571c9ec164e9dee22e339c869f85b228
rob <r.nilshilliard@gmail.com>
2014-04-22 21:08:05 +0200

    Started

---
+++ Breakout/Main.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Breakout
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonStart = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonStop = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonStart,
+            this.toolStripButtonStop});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(657, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonStart
+            // 
+            this.toolStripButtonStart.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonStart.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonStart.Image")));
+            this.toolStripButtonStart.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonStart.Name = "toolStripButtonStart";
+            this.toolStripButtonStart.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonStart.Text = "Start";
+            this.toolStripButtonStart.Click += new System.EventHandler(this.toolStripButtonStart_Click);
+            // 
+            // toolStripButtonStop
+            // 
+            this.toolStripButtonStop.Name = "toolStripButtonStop";
+            this.toolStripButtonStop.Size = new System.Drawing.Size(23, 22);
+            // 
+            // Main
+            // 
+            this.ClientSize = new System.Drawing.Size(657, 307);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Main";
+            this.Text = "Breakout";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Main_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Main_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Main_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Main_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Main_MouseUp);
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonStart;
+        private System.Windows.Forms.ToolStripButton toolStripButtonStop;
+    }
+}
+
---
+++ Breakout/Main.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Drawing.Drawing2D;
+
+namespace Breakout
+{
+    public partial class Main : Form
+    {
+        private Matrix m; // Matrix of transformations
+        private float s1; // scale
+        private int leftpaddlex = -10;
+        private int ballx = -3;
+        private int bally = -3;
+        private int xinc = 1;
+        private int yinc = 1;
+        private Point prev, next; 
+
+        public Main()
+        {
+            InitializeComponent();
+
+            //mouse points 
+            prev = new Point();
+            next = new Point();
+            //setup coordinate transformation 
+            float width = this.ClientRectangle.Width;
+            float height = this.ClientRectangle.Height;
+            float x1d = width * 0.1f;
+            float x2d = height * 0.9f;
+            float x3d = width * 0.9f;
+            float x4d = height * 0.1f;
+            float x1 = -80, x2 = -50, x3 = 80, x4 = 50;
+            s1 = (x1d - x3d) / (x1 - x3);
+            float s2 = (x2d - x4d) / (x2 - x4);
+            float t1 = (x1 * x3d - x1d * x3) / (x1 - x3);
+            float t2 = (x2 * x4d - x2d * x4) / (x2 - x4);
+            m = new Matrix();
+            m.Translate(t1, t2);
+            m.Scale(s1, s2); 
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            string s;
+        }
+
+        private void Main_Paint(object sender, PaintEventArgs e)
+        {
+            //do we need to calculate this every time? 
+
+            Graphics g = e.Graphics;
+            g.Transform = m;
+            using (Pen p = new Pen(Color.Black, 10 / s1))
+            {
+                //draw outline 
+                g.DrawRectangle(p, -80, -50, 160, 100);
+                //draw paddle 
+                g.DrawRectangle(p, leftpaddlex, -48, 20, 5);
+                //draw the ball 
+                g.FillEllipse(Brushes.Blue, ballx, bally, 6, 6);
+
+            }
+        }
+
+        private void Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    if (leftpaddlex > -80)
+                        leftpaddlex -= 1;
+                    break;
+                case Keys.Right:
+                    if (leftpaddlex < 60)
+                        leftpaddlex += 1;
+                    break;
+            } 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if ((ballx < -80) || (ballx > 74))
+                xinc *= -1;
+            if ((bally < -50) || (bally > 44))
+                yinc *= -1;
+            ballx += xinc;
+            bally += yinc;
+            this.Refresh(); 
+
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void toolStripButtonStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void toolStripButtonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void Main_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Capture = true;
+            prev.X = e.X;
+            prev.Y = e.Y;
+           
+        }
+
+        private void Main_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.Capture == true)
+            {
+                next.X = e.X;
+                next.Y = e.Y;
+                using (Graphics g = CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, prev, next);
+                }
+                prev.X = next.X;
+                prev.Y = next.Y;
+            } 
+
+        }
+
+        private void Main_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.Capture = false; 
+
+        }
+
+       
+
+
+    }
+}
